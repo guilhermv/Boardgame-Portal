33c0097d61cfb87ece5dfbb08e0dfda76da42beb
unknown <guiviego@gmail.com>
2021-06-19 13:06:28 -0300

    Added REST Api

---
+++ database/migrations/2021_06_19_151145_create_evaluations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEvaluationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evaluations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('player_id');
+            $table->integer('game_id');
+            $table->integer('rank');
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('evaluations');
+    }
+}
